338f413066d114f4a658258ef91b3be988592c9a
zobo24 <bozomarinovic5@gmail.com>
2024-12-10 22:08:14 +0100

    hamburger menu

--- rest_away_booking_project/src/components/navigation.tsx
+++ rest_away_booking_project/src/components/navigation.tsx
@@ -58,20 +58,35 @@ function processPage(page: Page, index: number, pathname: string) {
   );
 }
 
+function Hamburger() {
+  return (
+    <button
+      className="flex md:hidden flex-col justify-center items-end w-11 h-11 p-2 space-y-1.5 rounded-sm hover:bg-brand-stroke-weak active:bg-brand-stroke-weak"
+      aria-label="Open menu"
+    >
+      <span className="w-7 h-1 bg-brand-black rounded-full" />
+      <span className="w-4 h-1 bg-brand-black rounded-full" />
+      <span className="w-6 h-1 bg-brand-black rounded-full" />
+    </button>
+  );
+}
+
+
 export function Navigation() {
   const pathname = usePathname();
 
   return (
-    <nav className="flex justify-between items-center space-x-5 mt-8 mb-12 mr-12"> 
+    <nav className="container flex justify-between items-center space-x-5 mt-8 mb-12 mr-12"> 
       <Link href="/">
         <Logo className="text-3xl ml-12" />
       </Link>
       <ul
         className="flex justify-around space-x-12 mt-12 mb-4 mr-20 text-xl text-gray-500 "
       >
-        {" "}
         {pages.map((page, index) => processPage(page, index, pathname))}
       </ul>
+
+      <Hamburger/>
     </nav>
   );
 }
