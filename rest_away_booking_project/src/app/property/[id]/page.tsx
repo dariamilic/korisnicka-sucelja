68e80be6bdd10daf411119b65c53a413d2d4c597
zobo24 <bozomarinovic5@gmail.com>
2024-12-18 18:40:37 +0100

    navigation added to the page [id]

--- rest_away_booking_project/src/app/property/[id]/page.tsx
+++ rest_away_booking_project/src/app/property/[id]/page.tsx
@@ -2,44 +2,45 @@ import Link from "next/link";
 import type { Post } from "@/lib/api";
 import { ArrowLeft } from "lucide-react";
 import { Metadata } from "next";
-import Property from '../page';
+import Property from "../page";
 import { notFound } from "next/navigation";
+import { PropertyWrapper } from "@/app/property/_components/PropertyWrapper";
 
 export const metadata: Metadata = {
   title: "Property",
 };
 
 type BlogPostProps = {
-    params: { id: string };
-  };
-
+  params: { id: string };
+};
 
 async function getPost(id: string): Promise<Post> {
-    const data = await fetch(`${process.env.BASE_API_URL}/posts/${id}`);
-    return data.json();
-  }
+  const data = await fetch(`${process.env.BASE_API_URL}/posts/${id}`);
+  return data.json();
+}
 
 export default async function BlogPost({ params }: BlogPostProps) {
-    const post = await getPost(params.id);
-    const { id, title, body } = post;
-    if (!id) {
-        notFound();
-      }
-    return (
-      <main className="flex min-h-screen flex-col items-center p-10">
-        <article className="w-full max-w-2xl bg-white shadow-lg rounded-lg overflow-hidden p-6">
-        <Link
-          href="/property"
-          className="inline-flex items-center text-gray-600 hover:text-gray-900 transition-colors duration-200 mb-6"
-        >
-          <ArrowLeft className="mr-2 h-4 w-4" /> Back to all posts
-        </Link>
-        <h1 className="text-3xl md:text-4xl font-extrabold tracking-tight text-gray-900 mb-4">
-          Property {id}: {title}
-        </h1>
-        <p>{body}</p>
-      </article>
-       
-      </main>
-    );
+  const post = await getPost(params.id);
+  const { id, title, body } = post;
+  if (!id) {
+    notFound();
   }
+  return (
+    <PropertyWrapper>
+      <main className="flex min-h-screen flex-col items-center p-10">
+        <article className="w-full max-w-2xl bg-gray-200 shadow-lg rounded-lg overflow-hidden p-6">
+          <Link
+            href="/property"
+            className="inline-flex items-center text-gray-600 hover:text-gray-900 transition-colors duration-200 mb-6"
+          >
+            <ArrowLeft className="mr-2 h-4 w-4" /> Back to all properties
+          </Link>
+          <h1 className="text-3xl md:text-4xl font-extrabold tracking-tight text-gray-900 mb-4">
+            Property {id}: {title}
+          </h1>
+          <p>{body}</p>
+        </article>
+      </main>
+    </PropertyWrapper>
+  );
+}
