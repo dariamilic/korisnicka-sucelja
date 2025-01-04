0cfed991e72c4b6f02caac40039afc385740beb8
dariamilic <dariamilic76@gmail.com>
2025-01-04 14:33:59 +0100

    account page-profile setting up

--- rest_away_booking_project/src/app/account/profile/page.tsx
+++ rest_away_booking_project/src/app/account/profile/page.tsx
@@ -1,7 +1,58 @@
+'use client';
+import './profile.css';
+import { Navigation } from "@/components/navigation";
+import { useState } from "react";
+
 export default function AccountProfilePage() {
-    return (
-      <main className="flex min-h-screen flex-col items-center p-10">
-        <h1 className="text-6xl font-extrabold tracking-tight">Profile</h1>
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      {/* Navigacija */}
+      <Navigation setIsOpen={setIsOpen} />
+      
+      {/* Glavni sadržaj stranice */}
+      <main className="profile-container">
+        <h1 className="title">General Profile</h1>
+        <p className="description">
+          Change your profile picture and adjust your account information.
+        </p>
+
+        {/* Sekcija za profilnu sliku */}
+        <div className="profile-section">
+          <div className="profile-picture">
+            <img src="/emily.png" alt="Profile" />
+            <button className="update-btn">Update</button>
+          </div>
+        </div>
+
+         {/* Input Fields */}
+         <div className="profile-info">
+          <div className="input-group">
+          <div className="input-container">
+            <label htmlFor="name">Name</label>
+            <input id="name" type="text" placeholder="Emily" />
+          </div>
+          <button className="update-btn">Update</button>
+        </div>
+
+        <div className="input-group">
+          <div className="input-container">
+            <label htmlFor="email">Mail</label>
+            <input id="email" type="email" placeholder="Emily@company.com" />
+          </div>
+          <button className="update-btn">Update</button>
+        </div>
+
+        <div className="input-group">
+          <div className="input-container">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="password" placeholder="********" />
+          </div>
+          <button className="update-btn">Update</button>
+        </div>
+        </div>
       </main>
-    );
-  }
+    </>
+  );
+}
