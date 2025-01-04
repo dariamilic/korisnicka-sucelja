743ad3a50bb8ad584a7d5ef4e6ac04b7a1bf04bf
dariamilic <dariamilic76@gmail.com>
2025-01-04 17:57:48 +0100

    changed tekst fonds

--- rest_away_booking_project/src/app/account/saved_properties/page.tsx
+++ rest_away_booking_project/src/app/account/saved_properties/page.tsx
@@ -14,7 +14,7 @@ export default function AccountSavedPropertiesPage() {
       
       <section className="bg-gray-100 py-12">
       <div className="container mx-auto px-4">
-        <h2 className="text-4xl font-bold text-center text-gray-700 mb-8">
+        <h2 className="text-4xl font-bold text-center text-brand-text-strong text-gray-500 mb-8">
           Saved property
         </h2>
         <div className="grid grid-cols-3 gap-8">
@@ -31,10 +31,10 @@ export default function AccountSavedPropertiesPage() {
               />
             </div>
             <div className="mt-4 text-center">
-              <p className="text-lg text-gray-700 font-semibold">Stan</p>
-              <p className="text-gray-500">Zagreb</p>
-              <p className="text-gray-500">Cijena: 100€</p>
-              <p className="text-gray-500">Datum: 10.01.2025.</p>
+              <p className="text-lg text-gray-700 font-semibold">Hrvatska</p>
+              <p className="text-gray-500">Price: 47€ </p>
+              <p className="text-gray-500">Location: Zagreb</p>
+              <p className="text-gray-500">Date: 10.01.2025.-11.02.2025.</p> 
             </div>
           </div>
 
@@ -51,10 +51,10 @@ export default function AccountSavedPropertiesPage() {
               />
             </div>
             <div className="mt-4 text-center">
-              <p className="text-lg text-gray-700 font-semibold">Makarska</p>
-              <p className="text-gray-500">Makarska</p>
-              <p className="text-gray-500">Cijena: 120€</p>
-              <p className="text-gray-500">Datum: 15.01.2025.</p>
+              <p className="text-lg text-gray-700 font-semibold">Hrvatska</p>
+              <p className="text-gray-500">Price: 350€</p>
+              <p className="text-gray-500">Location: Makarska</p>
+              <p className="text-gray-500">Date: 15.07.2025.-20.07.2025</p>
             </div>
           </div>
 
@@ -71,10 +71,29 @@ export default function AccountSavedPropertiesPage() {
               />
             </div>
             <div className="mt-4 text-center">
-              <p className="text-lg text-gray-700 font-semibold">Split</p>
-              <p className="text-gray-500">Split</p>
-              <p className="text-gray-500">Cijena: 150€</p>
-              <p className="text-gray-500">Datum: 20.01.2025.</p>
+              <p className="text-lg text-gray-700 font-semibold">Hrvatska</p>
+              <p className="text-gray-500">Price: 180€</p>
+              <p className="text-gray-500">Location: Split</p>
+              <p className="text-gray-500">Date: 20.08.2025.-22.08.2025</p>
+            </div>
+          </div>
+           {/* Fourth image - Dubrovnik */}
+           <div className="bg-white p-4 rounded-lg shadow-lg">
+            <div className="relative w-full h-48">
+              <Image
+                src="/jahorina.jpg"
+                alt="Jahorina"
+                fill
+                className="object-cover rounded-lg"
+                sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
+                priority={false}
+              />
+            </div>
+            <div className="mt-4 text-center">
+              <p className="text-lg text-gray-700 font-semibold">Bosna i Hercegovina</p>
+              <p className="text-gray-500">Price: 250€</p>
+              <p className="text-gray-500">Location: Jahorina</p>
+              <p className="text-gray-500">Date: 25.02.2025.-27.03.2025.</p>
             </div>
           </div>
         </div>
