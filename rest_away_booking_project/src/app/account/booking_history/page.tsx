31b9b3c0d7c6cf2858b85812f5911fca7390216e
dariamilic <dariamilic76@gmail.com>
2025-01-04 16:57:11 +0100

    added info on history page

--- rest_away_booking_project/src/app/account/booking_history/page.tsx
+++ rest_away_booking_project/src/app/account/booking_history/page.tsx
@@ -1,7 +1,17 @@
+'use client'
+import { Navigation } from "@/components/navigation";
+import { useState } from "react";
+
+
 export default function AccountBookingHistoryPage() {
+  const [isOpen, setIsOpen] = useState(false);
+
     return (
-      <main className="flex min-h-screen flex-col items-center p-10">
-        <h1 className="text-6xl font-extrabold tracking-tight">Booking History</h1>
+      <>
+      <Navigation setIsOpen={setIsOpen} />
+      <main className="flex min-h-screen flex-col items-center justify-center p-6 text-center">
+        <h1 className="text-2xl font-bold tracking-tight leading-normal">Sorry, but it looks like you haven't booked any accommodation yet...</h1>
       </main>
+      </>
     );
   }
