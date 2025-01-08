afe0ec7bf78d695378ddf66d2fb3d44fdf2c7bb8
zobo24 <bozomarinovic5@gmail.com>
2025-01-08 19:14:06 +0100

    side navigations updated

--- rest_away_booking_project/src/app/search/amenities/page.tsx
+++ rest_away_booking_project/src/app/search/amenities/page.tsx
@@ -1,7 +1,26 @@
+'use client';
+
+import { useState, useCallback } from "react";
+import { Navigation } from "@/components/navigation";
+import { SideNavigation } from '../_components/navigation';
+
 export default function SearchAmenitiesPage() {
+
+   const [isOpen, setIsOpen] = useState(false);
+  
+     // Wrap setIsOpen in useCallback
+     const handleSetIsOpen = useCallback((state: boolean) => {
+      setIsOpen(state);
+    }, []);
+  
     return (
+      <>
+        {/* Navigacija */}
+        <Navigation isOpen={isOpen} setIsOpen={handleSetIsOpen} />
+        <SideNavigation/>
       <main className="flex min-h-screen flex-col items-center p-10">
         <h1 className="text-6xl font-extrabold tracking-tight">Search Amenities Filter</h1>
       </main>
+      </>
     );
   }
--- rest_away_booking_project/src/app/search/location/page.tsx
+++ rest_away_booking_project/src/app/search/location/page.tsx
@@ -1,7 +1,25 @@
+'use client';
+
+import { useState, useCallback } from "react";
+import { Navigation } from "@/components/navigation";
+import { SideNavigation } from '../_components/navigation';
+
 export default function SearchLocationPage() {
-    return (
-      <main className="flex min-h-screen flex-col items-center p-10">
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Wrap setIsOpen in useCallback
+  const handleSetIsOpen = useCallback((state: boolean) => {
+   setIsOpen(state);
+ }, []);
+
+ return (
+   <>
+     {/* Navigacija */}
+     <Navigation isOpen={isOpen} setIsOpen={handleSetIsOpen} />  
+     <SideNavigation/> 
+     <main className="flex min-h-screen flex-col items-center p-10">
         <h1 className="text-6xl font-extrabold tracking-tight">Search Location Filter</h1>
       </main>
+      </>
     );
   }
