29fe6074aeda9f38f6036e7c3a728d3246c3a4ae
zobo24 <bozomarinovic5@gmail.com>
2025-02-14 18:02:17 +0100

    json file for property

--- rest_away_booking_project/src/lib/supabase.ts
+++ rest_away_booking_project/src/lib/supabase.ts
@@ -1,8 +1,13 @@
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-
-
+import { createClient } from "@supabase/supabase-js";
+//the first database for users
 export const supabase = createClientComponentClient({
   supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
   supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_KEY!,
 });
 
+// Second Supabase client for property data (Second database)
+export const supabaseProperty = createClient(
+   process.env.NEXT_PUBLIC_SUPABASE_URL_2!, // URL for the second database
+  process.env.NEXT_PUBLIC_SUPABASE_KEY_2! // Key for the second database
+);
